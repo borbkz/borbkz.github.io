9d41d3bc41679d7887b44818bc933eeb44a5b1b9
borbkz <borbkz@gmail.com>
2019-09-27 17:50:49 -0700

    f

--- assets/js/allservers.js
+++ assets/js/allservers.js
@@ -334,7 +334,7 @@ $(document).ready(function () {
             var steam_id = firstEntry["steam_id"];
 
             //hi rufus
-            if(getSteamIDSubstring(steam_id) === "84091052") {
+            if(getSteamIDSubstring(steam_id) === "84091052" && Math.floor(Math.random() * 4) == 1) {
                 new SpiderController({ 'minBugs': 5, 'maxBugs': 10, 'min_frames': 7 });
             }
 
