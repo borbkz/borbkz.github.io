db27977f0692d6af691abe298dd6f0e8b59634d7
borbkz <borbkz@gmail.com>
2019-10-05 14:27:50 -0700

    reduced threshold

--- assets/js/util.js
+++ assets/js/util.js
@@ -323,13 +323,3 @@ console.log("NO")
 	first = false;
 	return mapTable;
 }
-
-
-(function () {
-	let a = Math.ceil((Math.random() * 10));
-	if (a == 1){
-		new BugController({ 'minBugs': 3, 'maxBugs': 6, 'min_frames': 10 });
-	} if(a == 2){
-		new SpiderController({ 'minBugs': 1, 'maxBugs': 1, 'min_frames': 7 });
-	}
-})();
